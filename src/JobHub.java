2b4a614f5180c6bb937bd6a50884255d03d06a39
TSCN <franciscoubaldo512@gmail.com>
2018-10-04 23:29:18 -0400

    reformat

--- src/JobHub.java
+++ src/JobHub.java
@@ -18,6 +18,7 @@ public class JobHub
             case 1:
             {
                 System.out.println("Which city are you looking at today? \nNewark(1)\nJersey City(2)\nSeacaucus(3)\nWeehawken(4)\nRoseland(5)\nEdison(6)\nHoboken(7)\nAll(8)");
+
                 indeed(input.nextInt());
                 while(true)
                 {
@@ -34,7 +35,7 @@ public class JobHub
 
             default:
             {
-                System.out.println("Can't you follow directions?");
+                System.out.println("Come on, try again");
                 break;
             }
         }
@@ -42,55 +43,56 @@ public class JobHub
     public static void indeed(int choice)
     {
 
-
+        String jobTitle = "software+engineer";
         System.out.println("Here are your indeed job listings." + '\n');
         HashMap<String, String> cities = new HashMap<>();
         switch(choice)
         {
+
             case 1:
             {
-                cities.put("Newark", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Newark,+NJ&jlid=fc0723f9333e9830&sort=date");
+                cities.put("Newark", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Newark,+NJ&jlid=fc0723f9333e9830&sort=date");
                 break;
             }
             case 2:
             {
-                cities.put("Jersey City", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Jersey+City,+NJ&jlid=a8725547cbf3b706&sort=date");
+                cities.put("Jersey City", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Jersey+City,+NJ&jlid=a8725547cbf3b706&sort=date");
                 break;
             }
             case 3:
              {
-                cities.put("Seacaucus", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Secaucus,+NJ&jlid=9c2227695c505787&sort=date");
+                cities.put("Seacaucus", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Secaucus,+NJ&jlid=9c2227695c505787&sort=date");
                 break;
             }
             case 4:
             {
-                cities.put("Weehawken", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Weehawken%2C%20NJ&jlid=61753ac7ba77bf38&sort=date");
+                cities.put("Weehawken", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Weehawken%2C%20NJ&jlid=61753ac7ba77bf38&sort=date");
                 break;
             }
             case 5:
             {
-                cities.put("Roseland", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Roseland,+NJ&jlid=218092364ca6bfe9&sort=date");
+                cities.put("Roseland", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Roseland,+NJ&jlid=218092364ca6bfe9&sort=date");
                 break;
             }
             case 6:
             {
-                cities.put("Edison", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Edison,+NJ&jlid=6978f3e2f6212bdf&sort=date");
+                cities.put("Edison", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Edison,+NJ&jlid=6978f3e2f6212bdf&sort=date");
                 break;
             }
             case 7:
             {
-                cities.put("Hoboken", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Hoboken,+NJ&jlid=9b1805cecf09cc58&sort=date");
+                cities.put("Hoboken", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Hoboken,+NJ&jlid=9b1805cecf09cc58&sort=date");
                 break;
             }
             default:
             {
-                cities.put("Newark", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Newark,+NJ&jlid=fc0723f9333e9830&sort=date");
-                cities.put("Jersey City", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Jersey+City,+NJ&jlid=a8725547cbf3b706&sort=date");
-                cities.put("Seacaucus", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Secaucus,+NJ&jlid=9c2227695c505787&sort=date");
-                cities.put("Weehawken", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Weehawken%2C%20NJ&jlid=61753ac7ba77bf38&sort=date");
-                cities.put("Roseland", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Roseland,+NJ&jlid=218092364ca6bfe9&sort=date");
-                cities.put("Edison", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Edison,+NJ&jlid=6978f3e2f6212bdf&sort=date");
-                cities.put("Hoboken", "https://www.indeed.com/jobs?q=software+engineer&l=07109&rbl=Hoboken,+NJ&jlid=9b1805cecf09cc58&sort=date");
+                cities.put("Newark", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Newark,+NJ&jlid=fc0723f9333e9830&sort=date");
+                cities.put("Jersey City", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Jersey+City,+NJ&jlid=a8725547cbf3b706&sort=date");
+                cities.put("Seacaucus", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Secaucus,+NJ&jlid=9c2227695c505787&sort=date");
+                cities.put("Weehawken", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Weehawken%2C%20NJ&jlid=61753ac7ba77bf38&sort=date");
+                cities.put("Roseland", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Roseland,+NJ&jlid=218092364ca6bfe9&sort=date");
+                cities.put("Edison", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Edison,+NJ&jlid=6978f3e2f6212bdf&sort=date");
+                cities.put("Hoboken", "https://www.indeed.com/jobs?q="+jobTitle+"&rbl=Hoboken,+NJ&jlid=9b1805cecf09cc58&sort=date");
                 break;
             }
         }
